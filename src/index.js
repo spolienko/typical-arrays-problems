05267a8fb864304a6ab16a59fa56ae5a0a77aa89
Sofiya Polienko <sofie.polienko@gmail.com>
2021-02-13 21:53:00 +0500

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
 exports.min = function min (array) {
-  return 0;
+  let minValue = 0;
+  if (array && array.length > 0) {
+    minValue = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (minValue > array[i])
+            minValue = array[i];
+    }
+  }
+    return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxValue = 0;
+  if (array && array.length > 0) {
+  for (let i = 0; i < array.length; i++) {
+      if (maxValue < array[i])
+          maxValue = array[i];
+  }
+}
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgValue = 0;
+  if (array && array.length > 0) {
+    for (let i = 0; i < array.length; i++) {
+        avgValue += array[i];
+    }
+    avgValue /= array.length;
+  }
+    return avgValue;
 }
